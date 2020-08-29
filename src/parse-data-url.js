41348cfdf03b203dee602af70549439ff3c1ec60
evilebottnawi <sheo13666q@gmail.com>
2020-08-29 17:11:50 +0300

    refactor: code

--- src/parse-data-url.js
+++ src/parse-data-url.js
@@ -1,6 +1,41 @@
 import MIMEType from 'whatwg-mimetype';
-import { percentDecode } from 'whatwg-url';
 import { atob } from 'abab';
+
+function isASCIIHex(c) {
+  return (
+    (c >= 0x30 && c <= 0x39) ||
+    (c >= 0x41 && c <= 0x46) ||
+    (c >= 0x61 && c <= 0x66)
+  );
+}
+
+function percentDecodeBytes(input) {
+  const output = new Uint8Array(input.byteLength);
+  let outputIndex = 0;
+
+  for (let i = 0; i < input.byteLength; ++i) {
+    const byte = input[i];
+
+    if (byte !== 0x25) {
+      output[outputIndex] = byte;
+    } else if (
+      byte === 0x25 &&
+      (!isASCIIHex(input[i + 1]) || !isASCIIHex(input[i + 2]))
+    ) {
+      output[outputIndex] = byte;
+    } else {
+      output[outputIndex] = parseInt(
+        String.fromCodePoint(input[i + 1], input[i + 2]),
+        16
+      );
+      i += 2;
+    }
+
+    outputIndex += 1;
+  }
+
+  return output.slice(0, outputIndex);
+}
 
 export default function parseDataUrl(stringInput) {
   let parsedUrl;
@@ -38,7 +73,7 @@ export default function parseDataUrl(stringInput) {
 
   const encodedBody = input.substring(position);
 
-  let body = Buffer.from(percentDecode(Buffer.from(encodedBody, 'utf-8')));
+  let body = Buffer.from(percentDecodeBytes(Buffer.from(encodedBody, 'utf-8')));
 
   // Can't use /i regexp flag because it isn't restricted to ASCII.
   const mimeTypeBase64MatchResult = /(.*); *[Bb][Aa][Ss][Ee]64$/.exec(mimeType);
