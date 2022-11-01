9b4e0fb87ab5eb587ec9e4d636b6ef4fc6fd4076
Sachin Kumar <ersachin3112@gmail.com>
2022-11-01 18:31:55 +0530

    ci: add node v19 (#190)

--- test/loader.test.js
+++ test/loader.test.js
@@ -266,7 +266,16 @@ describe("source-map-loader", () => {
     });
     expect(codeFromBundle.map).toBeUndefined();
     expect(codeFromBundle.code).toMatchSnapshot("code");
-    expect(getWarnings(stats)).toMatchSnapshot("warnings");
+
+    if (process.version.startsWith("v19")) {
+      expect(getWarnings(stats)[0]).toContain(
+        `SyntaxError: Unexpected non-whitespace character after JSON at position 102`
+      );
+    } else {
+      expect(getWarnings(stats)[0]).toContain(
+        `SyntaxError: Unexpected string in JSON at position 102`
+      );
+    }
     expect(getErrors(stats)).toMatchSnapshot("errors");
   });
 
